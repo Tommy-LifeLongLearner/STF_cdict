8cc87d0826a5778e9470c44b3c05439f2f5fceb7
Saadi Toumi Fouad <saaditoumifouad@gmail.com>
2022-01-10 22:15:33 +0100

    Add colored print header content to stf-dict.h

--- stf-dict.h
+++ stf-dict.h
@@ -18,6 +18,28 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
+#include <stdarg.h>
+
+// STF_PRINTC_H
+#if defined _WIN32 || defined _WIN64
+  #include <windows.h>
+  #define GREY FOREGROUND_RED | FOREGROUND_BLUE | FOREGROUND_GREEN
+  #define YELLOW FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_INTENSITY
+  #define RED FOREGROUND_RED| FOREGROUND_INTENSITY
+  #define GREEN FOREGROUND_GREEN | FOREGROUND_INTENSITY
+  #define BLUE FOREGROUND_BLUE | FOREGROUND_INTENSITY
+  #define WHITE FOREGROUND_BLUE | FOREGROUND_GREEN | FOREGROUND_RED | FOREGROUND_INTENSITY
+#else
+  #define GREY 0
+  #define YELLOW 1
+  #define RED 2
+  #define GREEN 3
+  #define BLUE 4
+  #define WHITE 5
+#endif
+
+void printc(int color, char *output, ...);
+// end of STF_PRINTC_H
 
 ////////////////////////////////////////////////////////////////////////////////
 /// \brief enum type: the type of dict item
